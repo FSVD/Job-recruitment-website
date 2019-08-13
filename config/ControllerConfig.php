a606ff2ee4f7a168b9e0a938a2d8d868a9f25aee
Fabio Schettino <fabio.schettino.01@gmail.com>
2019-08-13 16:36:39 +0200

    first commit

---
+++ config/ControllerConfig.php
@@ -0,0 +1,30 @@
+<?php 
+
+//container to return load controllers
+/**
+* 
+* You have to list every new controller here
+* 
+*/
+$container['HomeController'] = function($container){
+	return new \App\Controllers\HomeController($container);
+};
+
+$container['AuthController'] = function($container){
+	return new \App\Controllers\Auth\AuthController($container);
+};
+
+$container['PasswordController'] = function($container){
+	return new \App\Controllers\Auth\PasswordController($container);
+};
+
+$container['UsersController'] = function($container){
+	return new \App\Controllers\UsersController($container);
+};
+
+$container['PostsController'] = function($container){
+	return new \App\Controllers\PostsController($container);
+};
+$container['RolesController'] = function($container){
+	return new \App\Controllers\RolesController($container);
+};
