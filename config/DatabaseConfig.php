a5a33e809fa54a428627fa20bebbaa3c56b90df1
FSVD <fabio.schettino.01@gmail.com>
2019-08-16 19:39:45 +0200

    configuration updated

--- config/DatabaseConfig.php
+++ config/DatabaseConfig.php
@@ -5,10 +5,13 @@ require('../vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::create('../');
 $dotenv->load();
 
-$host = getenv('HOST');
-$database = getenv('DATABASE');
-$username = getenv('USERNAME');
-$password = getenv('PASSWORD');
+define('DB_HOST', env('DB_HOST'));
+define('DB_NAME', env('DB_NAME'));
+define('DB_USER', env('DB_USER'));
+define('DB_PASSWORD', env('DB_PASSWORD'));
+define('DB_PORT', env('DB_PORT'));
+define('DB_CHARSET', env('DB_CHARSET'));
+define('DB_COLLATION', env('DB_COLLATION'));
 
 /**
 *
@@ -22,20 +25,21 @@ $app = new \Slim\App([
     //Database definition
     'db' => [
         'driver' => 'mysql',
-        'host'=> $host,
-        'database' => $database,
-        'username' => $username,
-        'password' => $password,
-        'charset' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
+        'host'=> DB_HOST,
+        'database' => DB_NAME,
+        'username' => DB_USER,
+        'password' => DB_PASSWORD,
+        'port' => DB_PORT,
+        'charset' => DB_CHARSET,
+        'collation' => DB_COLLATION,
         'prefix' => '',
     ],
     'cakeDB' => [
         'className' => 'Cake\Database\Connection',
         'driver' => 'Cake\Database\Driver\Mysql',
-        'database' => $database,
-        'username' => $username,
-        'password' => $password,
+        'database' => DB_NAME,
+        'username' => DB_USER,
+        'password' => DB_PASSWORD,
         'cacheMetadata' => false // If set to `true` you need to install the optional "cakephp/cache" package.
 
     ]
