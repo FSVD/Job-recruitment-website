9ca371a5192b977b9ead0528d4ae51c5ab1e718d
FSVD <fabio.schettino.01@gmail.com>
2019-08-17 14:20:01 +0200

    sending variables to view

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,22 +1,24 @@
 <?php
 
 namespace App\Controllers;
+
 use Slim\Views\Twig as View;
 use App\Models\User;
 //uncomment the below, to start using cakephp
 use Cake\ORM\TableRegistry;
 
-class HomeController extends Controller{
-	
+class HomeController extends Controller
+{
+    public function index($request, $response)
+    {
+        // Sending variable to view trough associative array.
+        // return $this->view->render($response, 'home.twig', array( 'fontAwesomeKitCode' => env('FONT_AWESOME_KIT_CODE')));
+        return $this->view->render($response, 'home.twig');
+    }
+    
 
-	public function index($request, $response){
-	
-	return $this->view->render($response,'home.twig');
-	}
-	
-
-	public function about($request, $response){
-	
-	return $this->view->render($response,'about.twig');
-	}
+    public function about($request, $response)
+    {
+        return $this->view->render($response, 'about.twig');
+    }
 }
