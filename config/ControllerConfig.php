7de172a1ba1c66bf5a7e5b66fe2ce858e127bd8b
FSVD <fabio.schettino.01@gmail.com>
2019-08-18 14:40:13 +0200

    countries controller added

--- config/ControllerConfig.php
+++ config/ControllerConfig.php
@@ -1,30 +1,29 @@
-<?php 
+<?php
 
 //container to return load controllers
 /**
-* 
+*
 * You have to list every new controller here
-* 
+*
 */
-$container['HomeController'] = function($container){
-	return new \App\Controllers\HomeController($container);
+$container['HomeController'] = function ($container) {
+    return new \App\Controllers\HomeController($container);
 };
-
-$container['AuthController'] = function($container){
-	return new \App\Controllers\Auth\AuthController($container);
+$container['AuthController'] = function ($container) {
+    return new \App\Controllers\Auth\AuthController($container);
 };
-
-$container['PasswordController'] = function($container){
-	return new \App\Controllers\Auth\PasswordController($container);
+$container['PasswordController'] = function ($container) {
+    return new \App\Controllers\Auth\PasswordController($container);
 };
-
-$container['UsersController'] = function($container){
-	return new \App\Controllers\UsersController($container);
+$container['UsersController'] = function ($container) {
+    return new \App\Controllers\UsersController($container);
 };
-
-$container['PostsController'] = function($container){
-	return new \App\Controllers\PostsController($container);
+$container['PostsController'] = function ($container) {
+    return new \App\Controllers\PostsController($container);
 };
-$container['RolesController'] = function($container){
-	return new \App\Controllers\RolesController($container);
+$container['RolesController'] = function ($container) {
+    return new \App\Controllers\RolesController($container);
+};
+$container['CountriesController'] = function ($container) {
+    return new \App\Controllers\CountriesController($container);
 };
