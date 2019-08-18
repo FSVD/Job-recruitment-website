449d0e88cef3179bdfa690b67b0e63ed3257d8d8
FSVD <fabio.schettino.01@gmail.com>
2019-08-18 19:08:58 +0200

    belongs to country relation added

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,46 +1,53 @@
-<?php 
+<?php
 
 
 namespace App\Models;
 
- 
 /**
-* Extend eloquent model class 
+* Extend eloquent model class
 * read more https://laravel.com/docs/5.1/eloquent
 */
 use Illuminate\Database\Eloquent\Model;
+
 /**
 * To enable CakePHP's ORM, uncomment the line below
-* Read more => https://book.cakephp.org/3.0/en/orm.html 
+* Read more => https://book.cakephp.org/3.0/en/orm.html
 */
 
-//use Cake\ORM\TableRegistry; 
+//use Cake\ORM\TableRegistry;
 
 
 class User extends Model
 {
-	
-	//optional: define table name if different from 'users'
-	protected $table = 'users';
-	
-	protected $fillable = [
-		'id',
-		'first_name',
-		'last_name',
-		'email',
-		'password',
-		'phone',
-		'gender',
-	];
-	
-	public function setPassword($password){
-		$this->update([
-			'password' => password_hash($password, PASSWORD_DEFAULT),
-		]);
-	}
-	//every user belongs to a role
-	public function role(){
-		return $this->belongsTo('App\Models\Role');
-	}
-	
+    
+    //optional: define table name if different from 'users'
+    protected $table = 'users';
+    
+    protected $fillable = [
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'phone',
+        'gender',
+    ];
+    
+    public function setPassword($password)
+    {
+        $this->update([
+            'password' => password_hash($password, PASSWORD_DEFAULT),
+        ]);
+    }
+    //every user belongs to a role
+    public function role()
+    {
+        return $this->belongsTo('App\Models\Role');
+    }
+    
+    //every user belongs to a country
+    public function country()
+    {
+        return $this->belongsTo('App\Models\Country');
+    }
 }
