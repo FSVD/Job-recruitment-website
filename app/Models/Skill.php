f6f8177bc1f31d778fd9f1005ceb26827ab88543
Fabio Schettino <fabio.schettino.01@gmail.com>
2019-08-19 13:26:09 +0200

    skill model added

---
+++ app/Models/Skill.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Models;
+
+/**
+* Extend eloquent model class
+* read more https://laravel.com/docs/5.1/eloquent
+*/
+use Illuminate\Database\Eloquent\Model;
+
+/**
+* To enable CakePHP's ORM, uncomment the line below
+* Read more => https://skill.cakephp.org/3.0/en/orm.html
+*/
+
+//use Cake\ORM\TableRegistry;
+
+
+class Skill extends Model
+{
+    
+    //optional: define database table name if different from 'skills'
+    protected $table = 'skills';
+    // public $timestamps = false;
+    
+    protected $fillable = [
+        'id',
+        'name',
+        'url',
+        'description',
+        'user_id'
+    ];
+
+    /**
+    * Every skill belongs to a user.
+    * That is,  skills table has a user_id column which refers to the id of the user
+    * So let's define the relationship below
+    */
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
